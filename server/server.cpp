22989cae2d2671df8665d1d44b29b75bb4990ff1
Dmitriy Alimov <successdm13@gmail.com>
2022-12-30 15:15:53 +0500

    first commit

---
+++ server/server.cpp
@@ -0,0 +1,55 @@
+#include "server.h"
+
+Server::Server()
+{
+    if(this->listen(QHostAddress::Any, 2323))
+    {
+        qDebug() << "start";
+    }
+    else
+    {
+        qDebug() << "error";
+    }
+}
+
+void Server::incommingConnection(qintptr socketDescriptor)
+{
+    socket = new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead, this, &Server::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "client connected" << socketDescriptor;
+}
+
+void Server::slotReadyRead()
+{
+    socket = (QTcpSocket*)sender();
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_3);
+    if(in.status() == QDataStream::Ok)
+    {
+        qDebug() << "read...";
+        QString str;
+        in >> str;
+        qDebug() << str;
+        SendToClient(str);
+    }
+    else
+    {
+        qDebug() << "DataStream error";
+    }
+}
+
+void Server::SendToClient(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_3);
+    out << str;
+    for(int i = 0; i < Sockets.size(); i++)
+    {
+        Sockets[i]->write(Data);
+    }
+}
